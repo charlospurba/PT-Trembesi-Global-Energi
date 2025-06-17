213c10579f5f68c7758ca41bf3be0ab32ec8cf56
marthayulinda <marthayulindatobing@gmail.com>
2025-06-17 10:42:11 +0700

    Add project_kode coloumn to db

--- app/Models/User.php
+++ app/Models/User.php
@@ -21,6 +21,7 @@ class User extends Authenticatable
     'name',
     'email',
     'phone_number',
+    'project_kode',
     'npwp',
     'username',
     'password',
--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -19,6 +19,7 @@ return new class extends Migration {
                 $table->string('name'); // tambahkan
                 $table->string('email')->unique(); // tambahkan
                 $table->string('phone_number')->nullable(); // tambahkan
+                $table->string('project_kode')->nullable(); // tambahkan
                 $table->string('npwp')->nullable(); // tambahkan
                 $table->string('nib')->nullable(); // tambahkan
                 $table->string('comp_profile')->nullable(); // tambahkan
