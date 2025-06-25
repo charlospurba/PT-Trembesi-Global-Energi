a896a95446430bcb8b5594d33937ea3cbb9d0552
marthayulinda <marthayulindatobing@gmail.com>
2025-06-26 00:36:10 +0700

    [BE] Only active user can login

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -13,30 +13,32 @@ class AuthController extends Controller
     }
 
     public function login(Request $request)
-{
-    $credentials = $request->only('username', 'password');
+    {
+        $credentials = $request->only('username', 'password');
 
-    if (Auth::attempt($credentials)) {
-        $user = Auth::user();
+        // Tambahkan status sebagai bagian dari kriteria login
+        $credentials['status'] = 'active';
 
-        switch ($user->role) {
-            case 'super_admin':
-                return redirect('/dashboard/superadmin');
-            case 'procurement':
-                return redirect('/dashboard/procurement');
-            case 'vendor':
-                return redirect('/dashboard/vendor');
-            case 'project_manager':
-                return redirect('/dashboard/productmanager');
-            default:
-                Auth::logout();
-                return redirect()->back()->withErrors(['role' => 'Invalid role']);
+        if (Auth::attempt($credentials)) {
+            $user = Auth::user();
+
+            switch ($user->role) {
+                case 'super_admin':
+                    return redirect('/dashboard/superadmin');
+                case 'procurement':
+                    return redirect('/dashboard/procurement');
+                case 'vendor':
+                    return redirect('/dashboard/vendor');
+                case 'project_manager':
+                    return redirect('/dashboard/productmanager');
+                default:
+                    Auth::logout();
+                    return redirect()->back()->withErrors(['role' => 'Invalid role']);
+            }
         }
+
+        return redirect()->back()->withErrors(['login' => 'Username, password, or status incorrect']);
     }
-
-    return redirect()->back()->withErrors(['login' => 'Username or password incorrect']);
-}
-
 
     public function logout(Request $request)
     {
