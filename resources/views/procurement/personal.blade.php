0d50e38c3c1645dc05e388eab9383b40fccdb222
Gracesia.mrbn <gracesiaromauliix.9@gmail.com>
2025-07-16 16:45:29 +0700

    bagusin

--- resources/views/procurement/personal.blade.php
+++ resources/views/procurement/personal.blade.php
@@ -1,4 +1,4 @@
-```blade
+
 @extends('layouts.app')
 
 @section('content')
@@ -57,7 +57,7 @@
             </div>
 
             <p class="text-gray-600 mb-4">Showing {{ count($products) }} of {{ count($products) }} products</p>
-            
+
             <!-- Product Grid -->
             <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4">
                 @forelse($products as $product)
