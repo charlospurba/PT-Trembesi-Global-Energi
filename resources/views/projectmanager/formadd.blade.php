9bf3c3870c626afc9b74efe058a1a221732ccc73
marthayulinda <marthayulindatobing@gmail.com>
2025-08-05 14:59:48 +0700

    Change item to Description Material

--- resources/views/projectmanager/formadd.blade.php
+++ resources/views/projectmanager/formadd.blade.php
@@ -84,13 +84,13 @@
                                                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
                                                     d="M7 20l4-16m2 16l4-16M6 9h14M4 15h14"></path>
                                             </svg>
-                                            <span>Item</span>
+                                            <span>Description Material</span>
                                             <span class="text-red-500">*</span>
                                         </span>
                                     </label>
                                     <input type="text" name="item" id="item" value="{{ old('item') }}"
                                         class="w-full border border-gray-300 rounded-xl px-4 py-3 transition-all duration-200 focus:outline-none focus:ring-2 focus:ring-red-500 focus:border-transparent hover:border-gray-400 group-hover:shadow-sm"
-                                        placeholder="Enter item name" required>
+                                        placeholder="Enter description material" required>
                                 </div>
 
                                 {{-- UOM --}}
